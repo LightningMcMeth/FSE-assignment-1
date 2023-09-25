06f3b4fa8528e68e67c1c4c0939a7c78b31687bb
LightningMcMeth <mhlibovytskyy@kse.org.ua>
2023-09-25 18:28:50 +0300

    commiting all the files

---
+++ FSE-assignment-1/json-data.cs
@@ -0,0 +1,12 @@
+﻿namespace FSE_assignment_1;
+
+public class json_data
+{
+    public string userId { get; }
+    public string nickname { get; }
+    public string firstName { get; }
+    public string lastName { get; }
+    public string registrationDate { get; }
+    public string lastSeenDate { get; }
+    public bool isOnline { get; }
+}
