a78949adc9bf2be2dd78726b8e61c94b0ef6533b
LightningMcMeth <mhlibovytskyy@kse.org.ua>
2023-09-29 00:33:12 +0300

    working on task #4. Most of the tests pass, however there is some unexpected behaviour with bigger time windows

--- FSE_assignment_1/Program.cs
+++ FSE_assignment_1/Program.cs
@@ -1,125 +1,131 @@
 ﻿using System.Globalization;
 using System.Text.Json;
 
-namespace MyNamespace
+namespace FSE_assignment_1
 {
-    
-}
-
-string jsonUrl = "https://sef.podkolzin.consulting/api/users/lastSeen?offset=";
-
-using (HttpClient client = new HttpClient())
-{
-    try
+    public class lastSeenOnlineProgram
     {
-        Console.WriteLine($"Current time: {DateTime.UtcNow}");
-
-        for (int i = 0; i <= 200; i += 20)
+        static async Task Main(string[] args)
         {
-            string jsonUrlOffest = offsetURL(i, jsonUrl);
-            
-            HttpResponseMessage response = await client.GetAsync(jsonUrlOffest);
-            
-            if (response.IsSuccessStatusCode)
+            string jsonUrl = "https://sef.podkolzin.consulting/api/users/lastSeen?offset=";
+
+            using (HttpClient client = new HttpClient())
             {
-                
-                string jsonContent = await response.Content.ReadAsStringAsync();
-                
-                var root = JsonSerializer.Deserialize<root>(jsonContent);
-
-                foreach (var user in root.data)
+                try
                 {
-                    string userOnlineStatus = calculateLastSeenOnline(user);
-                    Console.WriteLine(userOnlineStatus);
-                }
+                    DateTime currentTime = DateTime.UtcNow;
+                    Console.WriteLine($"Current time: {currentTime}");
 
+                    for (int i = 0; i <= 200; i += 20)
+                    {
+                        string jsonUrlOffset = offsetURL(i, jsonUrl);
+
+                        HttpResponseMessage response = await client.GetAsync(jsonUrlOffset);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string jsonContent = await response.Content.ReadAsStringAsync();
+                            
+                            var root = parseJsonData(jsonContent);
+
+                            foreach (var user in root.data)
+                            {
+                                string userOnlineStatus = calculateLastSeenOnline(user, currentTime);
+                                Console.WriteLine(userOnlineStatus);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Failed to retrieve JSON file. Cringe. Status code: {response.StatusCode}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Uh oh, error: {ex.Message}");
+                }
+            }
+        }
+
+        public static string offsetURL(int offset, string url)
+        {
+            return url + offset;
+        }
+
+        public static string calculateLastSeenOnline(JsonData user, DateTime currentTime)
+        {
+            if (!user.isOnline)
+            {
+                DateTime lastSeenDate = DateTime.Parse(user.lastSeenDate, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+                lastSeenDate = lastSeenDate.ToUniversalTime();
+
+                DateTime startOfDay = currentTime.Date;
+
+                TimeSpan startOfDayDT = lastSeenDate - startOfDay;
+                TimeSpan timeDifference = currentTime - lastSeenDate;
+
+                if (timeDifference <= TimeSpan.FromSeconds(30))
+                {
+                    return user.nickname + " was online just now";
+                }
+                else if (timeDifference <= TimeSpan.FromMinutes(1))
+                {
+                    return user.nickname + " was online less than a minute ago";
+                }
+                else if (timeDifference <= TimeSpan.FromHours(1))
+                {
+                    return user.nickname + " was online a couple of minutes ago";
+                }
+                else if (timeDifference <= TimeSpan.FromHours(2))
+                {
+                    return user.nickname + " was online an hour ago";
+                }
+                else if (timeDifference <= TimeSpan.FromHours(2) && timeDifference <= startOfDayDT)
+                {
+                    return user.nickname + " was online today";
+                }
+                else if (timeDifference >= TimeSpan.FromHours(2) && timeDifference >= startOfDayDT)
+                {
+                    return user.nickname + " was online yesterday";
+                }
+                else if (timeDifference <= TimeSpan.FromDays(7))
+                {
+                    return user.nickname + " was online this week";
+                }
+                else
+                {
+                    return user.nickname + " was online a long time ago";
+                }
             }
             else
             {
-                Console.WriteLine($"Failed to retrieve JSON file. Cringe. Status code: {response.StatusCode}");
+                Console.WriteLine($"{user.nickname} is online");
+                return user.nickname + " is online";
             }
         }
+
+        public static Root? parseJsonData(string jsonData)
+        {
+            return JsonSerializer.Deserialize<Root>(jsonData);
+        }
     }
-    catch (Exception ex)
+    
+    //I know these are meant to be in separate files, which I have, but I couldn't get it to work
+    //I think it's due to some solution naming issue, since I had to rename it.
+    public class JsonData
     {
-        Console.WriteLine($"Uh oh, error: {ex.Message}");
+        public string userId { get; set; }
+        public string nickname { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string registrationDate { get; set; }
+        public string lastSeenDate { get; set; }
+        public bool isOnline { get; set; }
     }
-}
 
-
-string offsetURL(int offset, string url)
-{
-    return url + offset;
-}
-
-string calculateLastSeenOnline(json_data user)
-{
-    if (!user.isOnline)
+    public class Root
     {
-        //DateTime lastSeenDate = DateTime.ParseExact(user.lastSeenDate, "yyyy-MM-ddTHH:mm:ss.fffffffzzz",
-        //CultureInfo.InvariantCulture, DateTimeStyles.None);
-        DateTime lastSeenDate = DateTime.Parse(user.lastSeenDate, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
-        lastSeenDate = lastSeenDate.ToUniversalTime();
-        
-        DateTime startOfDay = DateTime.UtcNow.Date;
-
-        TimeSpan startOfDayDT = lastSeenDate - startOfDay;
-        TimeSpan timeDifference = DateTime.UtcNow - lastSeenDate;
-
-        if (timeDifference <= TimeSpan.FromSeconds(30))
-        {
-            return user.nickname + " was online just now";
-        }
-        else if (timeDifference <= TimeSpan.FromMinutes(1))
-        {
-            return user.nickname + " was online less than a minute ago";
-        }
-        else if (timeDifference <= TimeSpan.FromHours(1))
-        {
-            return user.nickname + " was online a couple of minutes ago";
-        }
-        else if (timeDifference <= TimeSpan.FromHours(2))
-        {
-            return user.nickname + " was online an hour ago";
-        }
-        else if (timeDifference <= TimeSpan.FromHours(2) && timeDifference <= startOfDayDT)
-        {
-            return user.nickname + " was online today";
-        }
-        else if (timeDifference >= TimeSpan.FromHours(2) && timeDifference >= startOfDayDT)
-        {
-            return user.nickname + " was online just now";
-        }
-        else if (timeDifference <= TimeSpan.FromDays(7))
-        {
-            return user.nickname + " was online this week";
-        }
-        else
-        {
-            return user.nickname + " was online a long time ago";
-        }
-
+        public int total { get; set; }
+        public List<JsonData> data { get; set; }
     }
-    else
-    {
-        Console.WriteLine($"{user.nickname} is online");
-        return user.nickname + "is online";
-    }
-}
-
-public class json_data
-{
-    public string userId { get; set; }
-    public string nickname { get; set; }
-    public string firstName { get; set; }
-    public string lastName { get; set; }
-    public string registrationDate { get; set; }
-    public string lastSeenDate { get; set; }
-    public bool isOnline { get; set; }
-}
-
-public class root
-{
-    public int total { get; set; }
-    public List<json_data> data { get; set; }
 }
--- Last_seen_online_tests/UnitTest1.cs
+++ Last_seen_online_tests/UnitTest1.cs
@@ -1,10 +1,227 @@
-namespace Last_seen_online_tests;
+using FSE_assignment_1;
 
-public class UnitTest1
+namespace UnitTest1;
+
+
+public class lastSeenOnlineTests
 {
     [Fact]
-    public void Test1()
+    public void offset_IsJoinedWithValidValue_true()
     {
+        string jsonUrl = "https://sef.podkolzin.consulting/api/users/lastSeen?offset=";
+        string jsonUrlWithOffset = "https://sef.podkolzin.consulting/api/users/lastSeen?offset=20";
+
+        string jsonUrlOffset = lastSeenOnlineProgram.offsetURL(20, jsonUrl);
         
+        Assert.Equal(jsonUrlWithOffset, jsonUrlOffset);
+    }
+
+    [Fact]
+    public void user_isOnline_true()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = true,
+        };
+        //don't really need DateTime here, but I need to pass some value into the function, even if it's not used
+        string datetimeStr = "2023-09-28T17:16:38.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 is online";
+
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+
+    }
+
+    [Fact]
+    public void user_isOnline_justNow()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-28T17:16:38.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online just now";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_lessThanAMinuteAgo()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-28T17:17:02.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online less than a minute ago";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_coupleOfMinutesAgo()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-28T17:20:28.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online a couple of minutes ago";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_anHourAgo()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-28T18:17:28.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online an hour ago";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_today()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-28T20:16:28.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online today";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_yesterday()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-27T17:16:28.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online yesterday";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_thisWeek()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-09-23T17:16:28.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online this week";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
+    }
+    
+    [Fact]
+    public void user_isOnline_longTimeAgo()
+    {
+        JsonData userData = new JsonData
+        {
+            userId = "e9de6dd1-84e5-9833-59de-8c51008de6a0",
+            nickname = "Emmett82",
+            firstName = "Emmett",
+            lastName = "Block",
+            registrationDate = "2023-06-08T21:50:41.1053254+00:00",
+            lastSeenDate = "2023-09-28T17:16:28.4068456+00:00",
+            isOnline = false,
+        };
+        string datetimeStr = "2023-08-28T17:16:28.3367009+00:00";
+        DateTime datetimeComparison = DateTime.Parse(datetimeStr);
+        datetimeComparison = datetimeComparison.ToUniversalTime();
+        string expectedStatus = "Emmett82 was online a long time ago";
+        
+        string userOnlineStatus = lastSeenOnlineProgram.calculateLastSeenOnline(userData, datetimeComparison);
+        
+        Assert.Equal(expectedStatus, userOnlineStatus);
     }
 }
